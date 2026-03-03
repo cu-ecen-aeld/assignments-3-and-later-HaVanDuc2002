e5928004f435d56b85615504dd45d4053350d1b2
Duc Ha Van <duc.havan@qorvo.com>
2026-03-03 15:24:34 +0700

    assignment 4 part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h> 
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -14,7 +17,25 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    if (thread_func_args == NULL) {
+        ERROR_LOG("Thread parameter is NULL");
+        return NULL;
+    }
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    int ret_mutex_lock = pthread_mutex_lock(thread_func_args->mutex);
+    if (ret_mutex_lock != 0) {
+        ERROR_LOG("Failed to lock mutex");
+        return NULL;
+    }
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+    int ret_mutex_unlock = pthread_mutex_unlock(thread_func_args->mutex);
+    if (ret_mutex_unlock != 0) {
+        ERROR_LOG("Failed to unlock mutex");
+        return NULL;
+    }
+    thread_func_args->thread_complete_success = true;
+    return thread_func_args;
 }
 
 
@@ -28,6 +49,21 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
-}
+    struct thread_data *thread_func_args = (struct thread_data *) malloc(sizeof(struct thread_data));
+    if (thread_func_args == NULL) {
+        return false;
+    }
+    thread_func_args->mutex = mutex;
+    thread_func_args->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_func_args->wait_to_release_ms = wait_to_release_ms;
+    thread_func_args->thread_complete_success = false;
 
+    int ret = pthread_create(thread, NULL, threadfunc, (void *)thread_func_args);
+    if (ret != 0) {
+        free(thread_func_args);
+        ERROR_LOG("pthread_create failed: %d (%s)", ret, strerror(ret));
+        return false;
+    }
+
+    return true;
+}
